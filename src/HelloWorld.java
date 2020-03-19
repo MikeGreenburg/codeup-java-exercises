d205ef62c5c264c526d861d1632cdcd32ec48f09
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-03-19 16:13:51 -0500

    finished the java exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,107 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello");
-        System.out.print(" World!");
+//        System.out.print("Hello");
+//        System.out.print(" World!");
+
+        // 1. Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console.
+//        int myFavoriteNumber = 23;
+//        System.out.println(myFavoriteNumber);
+//        System.out.print("myFavoriteNumber: " + myFavoriteNumber + "\n");
+
+        // 2. Create a String variable named myString and assign a string value to it, then print the variable out to the console.
+//        String myString = "3.14159";
+//        System.out.println(myString);
+
+        // 3. Change your code to assign a character value to myString. What do you notice?
+//        String myString = 'c';
+//        System.out.println(myString);
+
+        // 4. Change your code to assign the value 3.14159 to myString. What happens?
+//        String myString = 3.14159;
+//        System.out.println(myString);
+
+        // 5. Declare an long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
+//        long myNumber = ;
+//        System.out.println();
+
+        // 6. Change your code to assign the value 3.14 to myNumber. What do you notice?
+//        long myNumber = 3.14;
+//        System.out.println(myNumber);
+
+        // 7. Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
+//        long myNumber = 123L;
+//        System.out.println(myNumber);
+
+        // 8. Change your code to assign the value 123 to myNumber.
+        //
+        //Why does assigning the value 3.14 to a variable declared as a long not compile, but assigning an integer value does?
+//        long myNumber = 123;
+//        System.out.println(myNumber);
+
+        // 9. Change your code to declare myNumber as a float. Assign the value 3.14 to it. What happens? What are two ways we could fix this?
+//        float myNumber = 3.14;
+//        System.out.println(myNumber);
+
+        // 10. Copy and paste the following code blocks one at a time and execute them
+        //
+        //
+        //int x = 5;
+        //System.out.println(x++);
+        //System.out.println(x);
+        //
+        //int x = 5;
+        //System.out.println(++x);
+        //System.out.println(x);
+
+        // 11. Try to create a variable named class. What happens?
+//        class operation; is invalid.
+
+        // 12. Object is the most generic type in Java. You can assign any value to a variable of type Object. What do you think will happen when the following code is run?
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+        // 13. Rewrite the following expressions using the relevant shorthand assignment operators:
+        //
+        //
+        //int x = 4;
+        //x = x + 5;
+
+//        x = 4;
+//        x += 5;
+//        System.out.print(x);
+        //
+        //int x = 3;
+        //int y = 4;
+        //y = y * x;
+        //
+//        x = 3;
+//        y = 4;
+//        y *= 4;
+//        System.out.print(y, x);
+
+        //int x = 10;
+        //int y = 2;
+        //x = x / y;
+        //y = y - x;
+//        x = 10;
+//        y = 2;
+//        x /= y;
+//        y -= x;
+//        System.out.print(x, y);
+
+
+        // 14. What happens if you assign a value to a numerical variable that is larger (or smaller) than the type can hold? What happens if you increment a numeric variable past the type's capacity?
+        //
+        //Hint: Integer.MAX_VALUE is a class constant (we'll learn more about these later) that holds the maximum value for the int type.
+//        byte myByte = 0;
+//        int i = 0;
+//        while(i < 128) {
+//            myByte++;
+//            i++;
+//        }
+//        System.out.print(myByte);
     }
 }
+
