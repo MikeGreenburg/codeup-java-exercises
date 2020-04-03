a6d48b7e3e40d6d2b58991d9b3309c1803220135
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-04-03 11:30:15 -0500

    doing a general save

---
+++ src/Practice/Practice.java
@@ -0,0 +1,19 @@
+package Practice;
+
+public class Practice {
+    public static void main(String[] args) {
+        ifStatment("Hello");
+    }
+    //  Data Type       Name of variable       int/string etc.
+//        byte               testByte         =       5;
+//
+//        String testString
+    public static void ifStatment(String input){
+        if(input == "Hello"){
+            System.out.printf("%s", input);
+
+        }
+    }
+}
+
+
