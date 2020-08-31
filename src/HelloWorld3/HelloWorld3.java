adde299568d2402f87badb22b7e9435fd72e6cde
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-08-31 00:18:41 -0500

    refreshing on java

---
+++ src/HelloWorld3/HelloWorld3.java
@@ -0,0 +1,9 @@
+package HelloWorld3;
+
+public class HelloWorld3 {
+
+    public static void main(String [] args) {
+        //Print the words Hello World on the screen
+        System.out.println("Hello World");
+    }
+}
