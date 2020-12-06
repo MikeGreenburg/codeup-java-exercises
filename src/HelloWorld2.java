eef615ad00ee0a9bcc0badc9e004d61d3078f56c
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-12-06 17:49:49 -0600

    fixed some things

--- src/HelloWorld2.java
+++ src/HelloWorld2.java
@@ -1,6 +1,6 @@
 public class HelloWorld2 {
 
-    static String randomString;
+//    static String randomString;
 
     public static void main(String[] args) //always the function that executes first static means only a class can call for this function to execute and in the instance HelloWorld2
     {
