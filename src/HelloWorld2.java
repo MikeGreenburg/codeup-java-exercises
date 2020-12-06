89e0eaff22be28a54b6714acfdb05e42862f08a6
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-12-06 17:43:39 -0600

    adding capability for main method to be used

--- src/HelloWorld2.java
+++ src/HelloWorld2.java
@@ -2,7 +2,7 @@ public class HelloWorld2 {
 
     static String randomString;
 
-    static void main(String[] args) //always the function that executes first static means only a class can call for this function to execute and in the instance HelloWorld2
+    public static void main(String[] args) //always the function that executes first static means only a class can call for this function to execute and in the instance HelloWorld2
     {
         System.out.println("Hello World"); //variables are just a storage place for information
     }
